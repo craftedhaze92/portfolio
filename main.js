3f18733f80fe2170f7862fcc9f2364b9053f313f
DoHyunHwang <dohyun033@gmail.com>
2021-01-15 12:39:54 +0900

    JS: Navbar 추가

---
+++ main.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// 스크롤시 Navbar에 클래스 부여
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  console.log(window.scrollY);
+  console.log(`navbarHeight:${navbarHeight}`);
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
+});
